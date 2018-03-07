0e81414d4016adfa6c30e1d7ba79f876f64fe4ee
oshiro_seiya <oshiro_seiya@ca-adv.co.jp>
2018-03-07 16:07:42 +0900

    javascript から呼び出せるメソッド定義した

--- android/app/src/main/java/com/sample_android_native_modules/toast/ToastModule.java
+++ android/app/src/main/java/com/sample_android_native_modules/toast/ToastModule.java
@@ -4,6 +4,7 @@ import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -27,5 +28,10 @@ public class ToastModule extends ReactContextBaseJavaModule {
         constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
         constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
         return constants;
+    }
+
+    @ReactMethod
+    public void show(String message, int duration) {
+        Toast.makeText(getReactApplicationContext(), message, duration).show();
     }
 }
