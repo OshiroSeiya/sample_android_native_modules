d3179ddc85875fcd440b44b026a08aac2cb47d38
oshiro_seiya <oshiro_seiya@ca-adv.co.jp>
2018-03-07 15:41:47 +0900

    ReactContextBaseJavaModule を継承したNative Moduleクラスを生成

---
+++ android/app/src/main/java/com/sample_android_native_modules/toast/ToastModule.java
@@ -0,0 +1,10 @@
+package com.sample_android_native_modules.toast;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+
+public class ToastModule extends ReactContextBaseJavaModule {
+    public ToastModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+}
