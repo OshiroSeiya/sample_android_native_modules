eac895b4ff3e8b5459d35b4ceb741fd6ed42acc0
oshiro_seiya <oshiro_seiya@ca-adv.co.jp>
2018-03-07 15:53:56 +0900

    javascript から呼び出せる定数の設定した

--- android/app/src/main/java/com/sample_android_native_modules/toast/ToastModule.java
+++ android/app/src/main/java/com/sample_android_native_modules/toast/ToastModule.java
@@ -1,9 +1,17 @@
 package com.sample_android_native_modules.toast;
 
+import android.widget.Toast;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class ToastModule extends ReactContextBaseJavaModule {
+    private static final String DURATION_SHORT_KEY = "SHORT";
+    private static final String DURATION_LONG_KEY = "LONG";
+
     public ToastModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
@@ -11,5 +19,13 @@ public class ToastModule extends ReactContextBaseJavaModule {
     @Override
     public String getName() {
         return "ToastExample";
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+        constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+        return constants;
     }
 }
